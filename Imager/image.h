a3697b28152f9461d4ce41f207cd387e2fea29d4
Tony Cook <tony@develop=help.com>
2001-04-10 04:31:46 +0000

    added declarations for error handling code

--- Imager/image.h
+++ Imager/image.h
@@ -440,6 +440,27 @@ typedef struct {
 
 } symbol_table_t;
 
+/* error handling 
+   see error.c for documentation
+   the error information is currently global
+*/
+typedef struct {
+  char *msg;
+  int code;
+} i_errmsg;
+typedef void (*i_error_cb)(int code, char const *msg);
+typedef void (*i_failed_cb)(i_errmsg *msgs);
+extern i_error_cb i_set_error_cb(i_error_cb);
+extern i_failed_cb i_set_failed_cb(i_failed_cb);
+extern void i_set_argv0(char const *);
+extern int i_set_errors_fatal(int new_fatal);
+extern i_errmsg *i_errors();
+
+extern void i_push_error(int code, char const *msg);
+extern void i_push_errorf(int code, char const *fmt, ...);
+extern void i_push_errorvf(int code, char const *fmt, va_list);
+extern void i_clear_error();
+extern int i_failed(int code, char const *msg);
 
 
 #endif
