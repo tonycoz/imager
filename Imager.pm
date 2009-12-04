6936706bfb00908b8c261d99aa320e5868b7792a
Tony Cook <tony@develop=help.com>
2009-12-04 14:19:13 +0000

    0.71_03 release

--- Imager.pm
+++ Imager.pm
@@ -173,7 +173,7 @@ my %defaults;
 BEGIN {
   require Exporter;
   @ISA = qw(Exporter);
-  $VERSION = '0.71_02';
+  $VERSION = '0.71_03';
   eval {
     require XSLoader;
     XSLoader::load(Imager => $VERSION);
