d4056453521f666962b0760d465f901ed08aaf13
Tony Cook <tony@develop-help.com>
2012-01-20 22:29:35 +1100

    [rt #73906] add some other image processing modules to SEE ALSO

--- Imager.pm
+++ Imager.pm
@@ -4894,6 +4894,10 @@ L<Affix::Infix2Postfix>(3), L<Parse::RecDescent>(3)
 
 Other perl imaging modules include:
 
-L<GD>(3), L<Image::Magick>(3), L<Graphics::Magick>(3).
+L<GD>(3), L<Image::Magick>(3), L<Graphics::Magick>(3),
+L<Prima::Image>, L<IPA>.
+
+If you're trying to use Imager for array processing, you should
+probably using L<PDL>.
 
 =cut
