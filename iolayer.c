5ea2eee20830d0462d7b21e506664d77b8ed82f3
Tony Cook <tony@develop-help.com>
2022-05-03 15:29:38 +1000

    iolayer: avoid an unlikely NULL pointer dereference
    CID 185314

--- iolayer.c
+++ iolayer.c
@@ -573,12 +573,14 @@ i_io_peekn(io_glue *ig, void *buf, size_t size) {
       && !(ig->buf_eof || ig->error)) {
     i_io_read_fill(ig, size);
   }
-  
-  if (size > ig->read_end - ig->read_ptr)
-    size = ig->read_end - ig->read_ptr;
 
-  if (size)
-    memcpy(buf, ig->read_ptr, size);
+  if (ig->read_ptr && ig->read_end != ig->read_ptr) {
+    if (size > ig->read_end - ig->read_ptr)
+      size = ig->read_end - ig->read_ptr;
+
+    if (size)
+      memcpy(buf, ig->read_ptr, size);
+  }
   else if (ig->buf_eof) {
     IOL_DEB(fprintf(IOL_DEBs, "i_io_peekn() => 0 (eof)\n"));
     return 0;
