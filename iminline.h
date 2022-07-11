47fbc8855c103b80e74c5579d22378efd30749db
Tony Cook <tony@develop-help.com>
2022-07-11 21:06:51 +1000

    add iminline.h, inline functions for Imager

---
+++ iminline.h
@@ -0,0 +1,16 @@
+/*
+   Imager "functions" implemented as inline functions.
+*/
+#ifndef IMAGER_IMINLINE_H_
+#define IMAGER_IMINLINE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
