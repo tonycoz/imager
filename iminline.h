1c790b8dc2f01e0cb744255a5da99a8d684cb099
Tony Cook <tony@develop-help.com>
2022-08-01 19:49:12 +1000

    make the linear sample accessors into inline functions instead of macros

--- iminline.h
+++ iminline.h
@@ -8,6 +8,29 @@
 extern "C" {
 #endif
 
+IMAGER_STATIC_INLINE i_img_dim
+i_gslin(i_img *im, i_img_dim x, i_img_dim r, i_img_dim y,
+        i_sample16_t *samp, const int *chan, int chan_count) {
+  return (im->vtbl->i_f_gslin)(im, x, r, y, samp, chan, chan_count);
+}
+
+IMAGER_STATIC_INLINE i_img_dim
+i_gslinf(i_img *im, i_img_dim x, i_img_dim r, i_img_dim y,
+         i_fsample_t *samp, const int *chan, int chan_count) {
+  return (im->vtbl->i_f_gslinf)(im, x, r, y, samp, chan, chan_count);
+}
+
+IMAGER_STATIC_INLINE i_img_dim
+i_pslin(i_img *im, i_img_dim x, i_img_dim r, i_img_dim y,
+        const i_sample16_t *samp, const int *chan, int chan_count) {
+  return (im->vtbl->i_f_pslin)(im, x, r, y, samp, chan, chan_count);
+}
+
+IMAGER_STATIC_INLINE i_img_dim
+i_pslinf(i_img *im, i_img_dim x, i_img_dim r, i_img_dim y,
+                  const i_fsample_t *samp, const int *chan, int chan_count) {
+  return (im->vtbl->i_f_pslinf)(im, x, r, y, samp, chan, chan_count);
+}
 
 #ifdef __cplusplus
 }
