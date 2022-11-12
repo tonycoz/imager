f9a3029eb3f9dbc89528da42326a3213f5869664
Tony Cook <tony@develop-help.com>
2022-11-12 15:57:38 +1100

    add im_fatal() to the API

--- imexttypes.h
+++ imexttypes.h
@@ -286,6 +286,9 @@ typedef struct {
   (*f_im_lin_img_16_new_extra)(im_context_t, i_img_dim x, i_img_dim y, int ch, int extra);
   i_img *
   (*f_im_lin_img_double_new_extra)(im_context_t, i_img_dim x, i_img_dim y, int ch, int extra);
+  void
+  (*f_im_fatal)(im_context_t, int exit_code, const char *format, ...);
+
 } im_ext_funcs;
 
 #define PERL_FUNCTION_TABLE_NAME "Imager::__ext_func_table"
