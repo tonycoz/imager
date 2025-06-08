6a32e61d9df05596bc7ffb518539c95d2c4360ed
Tony Cook <tony@develop-help.com>
2025-06-08 18:15:02 +1000

    1.027_001 test release

--- Imager.pm
+++ Imager.pm
@@ -149,7 +149,7 @@ BEGIN {
   if ($ex_version < 5.57) {
     our @ISA = qw(Exporter);
   }
-  $VERSION = '1.027';
+  $VERSION = '1.027_001';
   require XSLoader;
   XSLoader::load(Imager => $VERSION);
 }
